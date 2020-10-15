00f2066ac321926a9734c032e258f6cac0350c4c
Geir <geir.hilmersen@gmail.com>
2020-10-15 16:17:32 +0200

    liten update

--- JS-Node/helloWorld.js
+++ JS-Node/helloWorld.js
@@ -1,8 +1,19 @@
 console.log("hello world");
 
-var isRunning = true;
 var counter = 0;
+
 while(counter < 20) {
   console.log(counter);
   counter = counter+1;
+}
+
+
+var isRunning = true;
+counter = 0;
+while(isRunning){
+  console.log(counter);
+  counter += 1;
+  if(counter >= 200000){
+    isRunning=false;
+  }
 }
